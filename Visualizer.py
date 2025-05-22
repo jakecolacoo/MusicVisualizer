a7c812512e546c76137dc14473ecfbf386bb59e0
jakecolacoo <94580156+jakecolacoo@users.noreply.github.com>
2025-05-22 17:55:07 -0600

    Create Visualizer.py

---
+++ Visualizer.py
@@ -0,0 +1,11 @@
+class Visualizer:
+    def __init__(self, processor):
+        self.processor = processor
+
+    def draw_waveform(self):
+        """Visualize the waveform."""
+        pass
+
+    def draw_spectrum(self):
+        """Visualize the frequency spectrum."""
+        pass
